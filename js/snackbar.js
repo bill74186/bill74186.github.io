08e0e3f95931fdceaa4367b1d42c1c0074c177f8
bill74186 <bill74186@outlook.com>
2026-06-14 14:06:42 +0000

    增加：增加了一篇文章

--- js/snackbar.js
+++ js/snackbar.js
@@ -18,53 +18,53 @@ var createSnackbar = (function() {
 */
   
   return function(config) {
-    var message = config.message,
-      actionText = config.actionText,
-      action = config.action,
-      duration = config.duration;
+	var message = config.message,
+	  actionText = config.actionText,
+	  action = config.action,
+	  duration = config.duration;
 
-    if (previous) {
-      previous.dismiss();
-    }
-    var snackbar = document.createElement('div');
-    snackbar.className = 'paper-snackbar';
-    snackbar.dismiss = function() {
-      this.style.opacity = 0;
-    };
-    var text = document.createTextNode(message);
-    snackbar.appendChild(text);
-    if (actionText) {
-      if (!action) {
-        action = snackbar.dismiss.bind(snackbar);
-      }
-      var actionButton = document.createElement('button');
-      actionButton.className = 'action';
-      actionButton.innerHTML = actionText;
-      actionButton.addEventListener('click', action);
-      snackbar.appendChild(actionButton);
-    }
-    setTimeout(function() {
-      if (previous === this) {
-        previous.dismiss();
-      }
-    }.bind(snackbar), duration || 5000);
-    
-    snackbar.addEventListener('transitionend', function(event, elapsed) {
-      if (event.propertyName === 'opacity' && this.style.opacity == 0) {
-        this.parentElement.removeChild(this);
-        if (previous === this) {
-          previous = null;
-        }
-      }
-    }.bind(snackbar));
+	if (previous) {
+	  previous.dismiss();
+	}
+	var snackbar = document.createElement('div');
+	snackbar.className = 'paper-snackbar';
+	snackbar.dismiss = function() {
+	  this.style.opacity = 0;
+	};
+	var text = document.createTextNode(message);
+	snackbar.appendChild(text);
+	if (actionText) {
+	  if (!action) {
+		action = snackbar.dismiss.bind(snackbar);
+	  }
+	  var actionButton = document.createElement('button');
+	  actionButton.className = 'action';
+	  actionButton.innerHTML = actionText;
+	  actionButton.addEventListener('click', action);
+	  snackbar.appendChild(actionButton);
+	}
+	setTimeout(function() {
+	  if (previous === this) {
+		previous.dismiss();
+	  }
+	}.bind(snackbar), duration || 5000);
+	
+	snackbar.addEventListener('transitionend', function(event, elapsed) {
+	  if (event.propertyName === 'opacity' && this.style.opacity == 0) {
+		this.parentElement.removeChild(this);
+		if (previous === this) {
+		  previous = null;
+		}
+	  }
+	}.bind(snackbar));
 
 
-    
-    previous = snackbar;
-    document.body.appendChild(snackbar);
-    // In order for the animations to trigger, I have to force the original style to be computed, and then change it.
-    getComputedStyle(snackbar).bottom;
-    snackbar.style.bottom = '0px';
-    snackbar.style.opacity = 1;
+	
+	previous = snackbar;
+	document.body.appendChild(snackbar);
+	// In order for the animations to trigger, I have to force the original style to be computed, and then change it.
+	getComputedStyle(snackbar).bottom;
+	snackbar.style.bottom = '0px';
+	snackbar.style.opacity = 1;
   };
 })();
